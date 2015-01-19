cd9af9648a7829fa1fffbaf2d8c809bf07d0f2e6
Yaozai Huang <yaozai4u@gmail.com>
2015-01-19 15:48:52 +0800

    Data.Table

--- Projects/Lectures/Data.R
+++ Projects/Lectures/Data.R
@@ -218,3 +218,104 @@ iris2 <-fromJSON(myjson)
 head(iris2)
 # http://www.r-bloggers.com/new-package-jsonlite-a-smarter-json-encoderdecoder/
 #         jsonlite vignette
+
+
+# Using data.table
+# Inherets from data.frame All functions that accept data.frame work on data.table
+# Written in C so it is much faster
+# Much much faster at subsetting, group, and updating
+
+install.packages("data.table")
+library(data.table)
+?data.table
+DF=data.frame(x=rnorm(9), y=rep(c("a","b","c"),each=3), z=rnorm(9))
+DF
+head(DF,3)
+
+DT= data.table(x=rnorm(9), y=rep(c("a","b","c"),each=3), z=rnorm(9))
+head(DT,3)
+
+# see all data tables in memory
+tables()
+
+DT[2,]
+DT[DT$y=='a',]
+
+# Subsetting rows
+DT[c(2,3)]
+
+# Subsetting columns
+DT[,c(2,3)]
+
+# Column subsetting in data.table
+{
+        x=1
+        y=2
+}
+k={print(10);5}
+
+print(k)
+
+Calculating values for variables with expressions
+
+DT[,list(mean(x),sum(z))]
+DT[,table(y)]
+
+# Adding new columns
+DT[,w:=z^2]
+DT
+
+DT2 <- DT
+DT[, y:= 2 ]
+head(DT, n=3)
+
+head(DT2, n=3)
+
+# Multiple operations
+
+DT[,m:= {tmp <- (x+z); log2(tmp+5)}]
+DT
+
+# plyr like operations
+DT[, a:= x>0]
+DT
+
+DT[,b:=mean(x+w),by=a]
+DT
+
+# Special variables
+# .N integer length 1, 
+set.seed(123);
+DT <- data.table(x=sample(letters[1:3],1E5,TRUE))
+DT
+
+DT[,.N,by=x]
+
+
+# Keys
+DT <- data.table(x=rep(c("a","b","c"),each=100),y=rnorm(300))
+setkey(DT,x)
+DT['a']
+
+DT
+
+# Joins
+DT1 <- data.table(x=c("a","a",'b','dt1'),y=1:4)
+DT2 <- data.table(x=c('a','b','dt2'),z=5:7)
+
+setkey(DT1, x); setkey(DT2, x)
+DT1; DT2
+merge(DT1,DT2)
+
+
+# Fast reading
+
+big_df <- data.frame(x=rnorm(1E6), y=rnorm(1E6))
+file <- tempfile()
+write.table(big_df, file=file, row.names=F, col.name=T, sep="\t", quote=F)
+system.time(fread(file))
+system.time(read.table(file, header=T,sep="\t"))
+
+
+# lastest contain new function like melt dcast for data.tables
+
